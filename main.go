9f0fada43407e5760789853348f795895114c98e
Katz Sakai <sakai@onflow.jp>
2025-03-24 07:14:19 +0000

    わかりやすく書き換えてみた

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 // see: https://mattn.kaoriya.net/software/lang/go/20140501172821.htm
 type pascalCaseWriter struct {
-	w    io.Writer
-	last byte
+	writer   io.Writer
+	prevChar byte // 1つ前に書き込んだ文字
 }
 
 func (w *pascalCaseWriter) Write(p []byte) (int, error) {
@@ -17,18 +17,18 @@ func (w *pascalCaseWriter) Write(p []byte) (int, error) {
 	var b [1]byte
 	for n, _ := range p {
 		b[0] = p[n]
-		switch w.last {
+		switch w.prevChar {
 		case ' ', '\t', '\r', '\n', 0:
 			if 'a' <= b[0] && b[0] <= 'z' {
 				b[0] -= 32
 			}
 		}
 
-		nw, err := w.w.Write(b[:])
+		nw, err := w.writer.Write(b[:])
 		if err != nil {
 			return r + nw, err
 		}
-		w.last = b[0]
+		w.prevChar = b[0]
 	}
 	return r, nil
 }
