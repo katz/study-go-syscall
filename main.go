47548d871fcc44c473fe14f6319539a4c9c920be
Katz Sakai <sakai@onflow.jp>
2025-03-24 08:08:28 +0000

    3.4.1標準入力

--- main.go
+++ main.go
@@ -6,38 +6,20 @@ import (
 	"os"
 )
 
-// see: https://mattn.kaoriya.net/software/lang/go/20140501172821.htm
-type pascalCaseWriter struct {
-	writer   io.Writer
-	prevChar byte // 1つ前に書き込んだ文字
-}
-
-func (w *pascalCaseWriter) Write(p []byte) (int, error) {
-	r := 0
-	var b [1]byte
-	for n, _ := range p {
-		b[0] = p[n]
-		switch w.prevChar {
-		case ' ', '\t', '\r', '\n', 0:
-			if 'a' <= b[0] && b[0] <= 'z' {
-				b[0] -= 32
-			}
-		}
-
-		nw, err := w.writer.Write(b[:])
-		if err != nil {
-			return r + nw, err
-		}
-		w.prevChar = b[0]
-	}
-	return r, nil
-}
-
-func NewPascalCaseWriter(w io.Writer) *pascalCaseWriter {
-	return &pascalCaseWriter{w, 0}
-}
-
 func main() {
-	w := NewPascalCaseWriter(os.Stdout)
-	fmt.Fprintln(w, "hello world")
+	// stdinからデータ読み込むコードを書く
+
+	for {
+		buffer := make([]byte, 5)
+		size, err := os.Stdin.Read(buffer)
+		if err == io.EOF {
+			fmt.Println("EOF")
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading from stdin:", err)
+			break
+		}
+		fmt.Printf("size=%d, buffer=%s\n", size, string(buffer))
+	}
 }
